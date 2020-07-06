0fef129fcf5e31d3c4288911c48556ab47a1d04d
Jsalvadorpp <josesalvadorpp@hotmail.com>
2020-07-06 00:05:53 -0400

    fixed icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,75 +5,63 @@ import { weatherIcons } from 'src/app/weather-icons/weather-icons';
 import { ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  encapsulation: ViewEncapsulation.None,
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.scss' ],
+	encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  constructor(private datePipe: DatePipe) {}
+	constructor(private datePipe: DatePipe) {}
 
-  title = 'weather-angular';
-  weather: any = {};
-  weather_icon: string;
-  iconsList = weatherIcons;
+	title = 'weather-angular';
+	weather: any = {};
+	weather_icon: string;
+	iconsList = weatherIcons;
 
-  ngOnInit(): void {}
+	ngOnInit(): void {}
 
-  retrieveData(weather): void {
-    this.weather = weather;
-    let currentTime = this.weather.dt;
-    let sunrise = this.weather.sys.sunrise;
-    let sunset = this.weather.sys.sunset;
-    let code = this.weather.weather[0].main;
+	retrieveData(weather): void {
+		this.weather = weather;
+		let currentTime = this.weather.dt;
+		let sunrise = this.weather.sys.sunrise;
+		let sunset = this.weather.sys.sunset;
+		let code = this.weather.weather[0].main;
 
-    const newDate: any = new Date();
-    const local_timezone_offset = new Date().getTimezoneOffset() * 60;
-    const fixedTime =
-      newDate - (-local_timezone_offset - weather.timezone) * 1000;
-    this.weather_icon = this.getWeatherIcon(
-      sunset,
-      sunrise,
-      code,
-      this.weather.dt
-    );
-    this.weather.dt = fixedTime;
+		const newDate: any = new Date();
+		const local_timezone_offset = new Date().getTimezoneOffset() * 60;
+		const fixedTime = newDate - (-local_timezone_offset - weather.timezone) * 1000;
+		this.weather_icon = this.getWeatherIcon(sunset, sunrise, code, this.weather.dt);
+		this.weather.dt = fixedTime;
 
-    //let currentTime = this.datePipe.transform(weather.location.localtime, 'a');
-    //let currentTime = new Date(weather.location.localtime).getTime();
-    //let latitude = weather.location.lat;
-    //let longitude = weather.location.lon;
-    //let code = weather.current.weather_code;
+		//let currentTime = this.datePipe.transform(weather.location.localtime, 'a');
+		//let currentTime = new Date(weather.location.localtime).getTime();
+		//let latitude = weather.location.lat;
+		//let longitude = weather.location.lon;
+		//let code = weather.current.weather_code;
 
-    //this.weather_icon = this.getWeatherIcon(latitude, longitude, code, currentTime);
-    //console.log(this.weather_icon);
-  }
+		//this.weather_icon = this.getWeatherIcon(latitude, longitude, code, currentTime);
+		//console.log(this.weather_icon);
+	}
 
-  getWeatherIcon(sunSet, sunRise, code, currentTime): string {
-    //let sunset = getSunset(latitude, longitude).getTime();
-    //let sunrise = getSunrise(latitude, longitude).getTime();
-    let sunset = sunSet;
-    let sunrise = sunRise;
+	getWeatherIcon(sunSet, sunRise, code, currentTime): string {
+		//let sunset = getSunset(latitude, longitude).getTime();
+		//let sunrise = getSunrise(latitude, longitude).getTime();
+		let sunset = sunSet;
+		let sunrise = sunRise;
 
-    let time = currentTime >= sunrise && currentTime < sunset ? 'day' : 'night';
-    this.setBackgroundColor(time);
+		let time = currentTime >= sunrise && currentTime < sunset ? 'day' : 'night';
+		this.setBackgroundColor(time);
 
-    let icon = this.iconsList[code][time];
-    let iconWrapper = document.querySelector('.icon-wrapper');
-    let spanElement = document.createElement('span');
-    spanElement.setAttribute('data-icon', icon);
+		let icon = this.iconsList[code][time];
 
-    iconWrapper.appendChild(spanElement);
+		return icon;
+	}
 
-    console.log(icon);
-    return icon;
-  }
+	setBackgroundColor(time: string): void {
+		let removeClass = time == 'day' ? 'night' : 'day';
+		let body = document.querySelector('body');
 
-  setBackgroundColor(time: string): void {
-    let removeClass = time == 'day' ? 'night' : 'day';
-    let body = document.querySelector('body');
-
-    body.classList.remove(removeClass);
-    body.classList.add(time);
-  }
+		body.classList.remove(removeClass);
+		body.classList.add(time);
+	}
 }
--- src/app/weather-icons/weather-icons.ts
+++ src/app/weather-icons/weather-icons.ts
@@ -1,62 +1,62 @@
 export const weatherIcons = {
 	Thunderstorm: {
-		day: 'wi:day-storm-showers',
-		night: 'wi:night-alt-storm-showers'
+		day: 'wi-day-storm-showers',
+		night: 'wi-night-alt-storm-showers'
 	},
 	Drizzle: {
-		day: 'wi:day-showers',
-		night: 'wi:night-alt-showers'
+		day: 'wi-day-showers',
+		night: 'wi-night-alt-showers'
 	},
 	Rain: {
-		day: 'wi:day-rain',
-		night: 'wi:night-alt-rain'
+		day: 'wi-day-rain',
+		night: 'wi-night-alt-rain'
 	},
 	Snow: {
 		day: 'snowflake-cold',
 		night: 'snowflake-cold'
 	},
 	Mist: {
-		day: 'wi:fog',
-		night: 'wi:fog'
+		day: 'wi-fog',
+		night: 'wi-fog'
 	},
 	Smoke: {
-		day: 'wi:smoke',
-		night: 'wi:smoke'
+		day: 'wi-smoke',
+		night: 'wi-smoke'
 	},
 	Haze: {
-		day: 'wi:fog',
-		night: 'wi:fog'
+		day: 'wi-fog',
+		night: 'wi-fog'
 	},
 	Dust: {
-		day: 'wi:dust',
-		night: 'wi:dust'
+		day: 'wi-dust',
+		night: 'wi-dust'
 	},
 	Fog: {
-		day: 'wi:fog',
-		night: 'wi:fog'
+		day: 'wi-fog',
+		night: 'wi-fog'
 	},
 	Sand: {
-		day: 'wi:dust',
-		night: 'wi:dust'
+		day: 'wi-dust',
+		night: 'wi-dust'
 	},
 	Ash: {
-		day: 'wi:fog',
-		night: 'wi:fog'
+		day: 'wi-fog',
+		night: 'wi-fog'
 	},
 	Squall: {
-		day: 'wi:fog',
-		night: 'wi:fog'
+		day: 'wi-fog',
+		night: 'wi-fog'
 	},
 	Tornado: {
-		day: 'wi:tornado',
-		night: 'wi:tornado'
+		day: 'wi-tornado',
+		night: 'wi-tornado'
 	},
 	Clear: {
-		day: 'wi:day-sunny',
-		night: 'wi:night-clear'
+		day: 'wi-day-sunny',
+		night: 'wi-night-clear'
 	},
 	Clouds: {
-		day: 'wi:cloudy',
-		night: 'wi:cloudy'
+		day: 'wi-cloudy',
+		night: 'wi-cloudy'
 	}
 };
